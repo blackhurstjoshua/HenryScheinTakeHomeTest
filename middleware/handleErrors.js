26e6acff99080729cbffebabff4d675d93e82658
Joshua <blackhurst.joshua@gmail.com>
2021-02-27 17:19:54 -0700

    Finished validation and functionality

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,17 +1,18 @@
-const { GeneralError } = require('../utils/errors')
 
-const handleErrors = (err, req, res, nesxt) => {
+const { GeneralError } = require('../utils/errors');
+
+const handleErrors = (err, req, res, next) => {
   if (err instanceof GeneralError) {
     return res.status(err.getCode()).json({
-      status: err.getCode(),
+      status: '400',
       message: err.message
-    })
+    });
   }
 
-  return res.status(err.getCode()).json({
-    status: err.getCode(),
+  return res.status(500).json({
+    status: '500',
     message: err.message
-  })
+  });
 }
 
 module.exports = handleErrors;
--- routes/person.js
+++ routes/person.js
@@ -1,35 +1,42 @@
 const router = require('express').Router()
-const { BadRequest } = require('../utils/errors')
+const { BadRequest, GeneralError } = require('../utils/errors')
 
-
-
-
-//  ! IMPORTANT: Review article on creating an error handling middleware!!!
-
-
-
-
-
-
-// Todo: Consider catching an invalid leap day birthday.
-// let dateObj = new Date()
-// console.log(dateObj)
+// ! IMPORTANT: Consider checking for if(index < 0) instead of if(index > -1)
+// ! IMPORTANT: MAKE IT LOOK NICE. Double check spacing, quotes, and semicolons.
+// Todo: Move person class and functions into seperate folders.
 
 let persons = []
 class Person {
   constructor(firstName, lastName, dateOfBirth, emailAddress, socialSecurityNumber) {
     this.firstName = firstName
     this.lastName = lastName
-    this.dateOfBirth = dateOfBirth
+    this.dateOfBirth = new Date(dateOfBirth)
     this.emailAddress = emailAddress
     this.socialSecurityNumber = socialSecurityNumber
+  }
+
+  validate() {
+    let letters = /^[a-zA-Z]+$/
+    let socials = /^(\d{3}-\d{2}-\d{4})+$/
+    console.log(socials.test(this.socialSecurityNumber))
+
+    if (this.firstName && this.lastName && this.dateOfBirth && this.emailAddress && this.socialSecurityNumber) {
+      if (letters.test(this.firstName) && letters.test(this.lastName)) {
+        if (this.dateOfBirth != "Invalid Date") {
+          if (this.emailAddress.indexOf('@') > -1) {
+            if (socials.test(this.socialSecurityNumber))
+              return true
+          }
+        }
+      }
+    }
   }
 }
 
 function findPerson(key) {
   let person = persons.find((per, index) => {
     if (per.socialSecurityNumber == key)
-      return true
+    return true
   })
   return person
 }
@@ -37,43 +44,54 @@ function findPerson(key) {
 function findIndex(key) {
   let index = persons.findIndex((per, index) => {
     if (per.socialSecurityNumber == key)
-      return true
+    return true
   })
   return index
 }
 
+function validSocial(key) {
+  let socials = /^(\d{3}-\d{2}-\d{4})+$/
+  return socials.test(key)
+}
+
 // ROUTES ==============================
 
-// ? Get (needs final check)
-router.get('/', (req, res) => {
+// * Get
+router.get('/', (req, res, next) => {
   try {
     if (!persons.length)
-      throw('persons array is empty')
+    throw new GeneralError('Persons array is empty.')
     res.status(200).json(persons)
   } catch(err) {
-    res.status(500).send(`Unknown server side error: ${err}`)
+    next(err)
   }
 })
 
-// ? Get one (needs final check)
-router.get('/:id', (req, res) => {
+// * Get One
+router.get('/:id', (req, res, next) => {
   try {
+    let valid = validSocial(req.params.id)
+    console.log(valid)
+    if (!valid)
+      throw new BadRequest('Invalid social security number')
+
     let person = findPerson(req.params.id)
     if (!person)
-      throw(`Person at ${req.params.id} does not exist`)
+      throw new BadRequest(`Person at ${req.params.id} does not exist`)
+
     res.status(200).json(person)
   } catch(err) {
-    res.status(400).json(err)
+    next(err)
   }
 })
 
-// ? Add new (field error handling and strict class usage : don't allow for duplicate person)
-router.post('/', async (req, res) => {
+// * Add new
+router.post('/', async (req, res, next) => {
   try {
     let soc = req.body.socialSecurityNumber
-    if (findIndex(soc) >= 0)
+    if (findIndex(soc) > -1)
       throw new BadRequest('User already exists')
-
+    
     let newPerson = await new Person(
       req.body.firstName,
       req.body.lastName,
@@ -81,25 +99,32 @@ router.post('/', async (req, res) => {
       req.body.emailAddress,
       req.body.socialSecurityNumber
     )
-
-    if (!newPerson.firstName)
-      throw new BadRequest('Missing first name')
+      
+    // ? Validate newPerson (consider return specific feedback)
+    let valid = newPerson.validate()
+    if (!valid)
+      throw new BadRequest("Invalid person syntax")
+      
     persons.push(newPerson)
     return res.status(201).json(newPerson)
   } catch (err) {
-    return res.status(400).send(err)
+    next(err)
   }
 })
-
-// ? Put
-router.put('/:id', async (req, res) => {
+  
+// * Put
+router.put('/:id', async (req, res, next) => {
   try {
     /*
-    Todo: 0. Make sure the social is not being changed, and that the fields are correct.
     *Todo: 1. Find the person and index. 
     *Todo: 2. Replace values (either individually or the whole thing). 
     *Todo: 3. Place the updated data where the old data was
     */
+
+    let validSoc = validSocial(req.params.id)
+    if (!validSoc)
+      throw new BadRequest('Invalid social security number')
+
     let personToChange = findPerson(req.params.id)
     let index = findIndex(req.params.id)
     let newPerson = await new Person(
@@ -109,29 +134,39 @@ router.put('/:id', async (req, res) => {
       req.body.emailAddress,
       personToChange.socialSecurityNumber
     )
-    persons.splice(index,1, newPerson)
-    
+
+    let valid = newPerson.validate()
+    if (!valid)
+      throw new BadRequest("Invalid person syntax")
+
+    persons.splice(index, 1, newPerson)
     res.status(200).json(newPerson)
   } catch(err) {
-
-  }
-  
+    next(err)
+  }  
 })
-
-// ? Delete (needs final check)
-router.delete('/:id', (req, res) => {
+    
+// * Delete
+router.delete('/:id', (req, res, next) => {
   try {
+    let valid = validSocial(req.params.id)
+    if (!valid)
+      throw new BadRequest('Invalid social security number')
+
     let index = findIndex(req.params.id)
     if(index < 0)
-      throw(`Could not find person with ${req.params.id} social.`)
+      throw new BadRequest('User does not exist')
+    
     persons.splice(index, 1)
-    res.status(200).send(`Person with ${req.params.id} social, has been deleted.`)
+    res.status(200).json({
+      status: "200",
+      message: `Person with ${req.params.id} social, has been deleted.`
+    })
   } catch(err) {
     res.status(400).send(err)
   }
-
 })
-
+    
 module.exports = router
 
 
@@ -142,3 +177,20 @@ let testPerson2 = new Person('Jack', 'Dexter', '11/03/2009', 'dexter.jack@gmail.
 let testPerson3 = new Person('Rachet', 'Clank', '11/04/2002', 'clank.rachet@gmail.com', '114-04-2002')
 let testPerson4 = new Person('Geralt', 'Rivia', '05/19/2015', 'rivia.geralt@gmail.com', '505-19-2015')
 persons.push(testPerson1, testPerson2, testPerson3, testPerson4)
+
+
+
+
+
+
+
+
+// GRAVEYARD =========================================
+/*
+console.log(letters.test(this.firstName))
+console.log(letters.test(this.lastName))
+console.log(this.dateOfBirth)
+console.log(this.emailAddress.indexOf('@'))
+console.log(socials.test(this.socialSecurityNumber))
+console.log('----')
+*/
